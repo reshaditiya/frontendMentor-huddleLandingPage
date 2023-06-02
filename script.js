9e4ca14c91fccb63d61d158f17a8776d5557d8d6
reshaditiya <reshaditiya@gmail.com>
2023-06-02 23:24:25 +0700

    layout fix

---
+++ script.js
@@ -0,0 +1,17 @@
+// UNOCSS
+window.__unocss = {
+	theme: {
+		colors: {
+			primaryViolet: "hsl(257, 40%, 49%)",
+			primaryMagenta: "hsl(300, 69%, 71%)",
+		},
+		fontFamily: {
+			poppins: "Poppins, sans-serif",
+			"open-sans": "'Open Sans', sans-serif",
+		},
+	},
+	shortcuts: {
+		"social-media":
+			"p-1.25 text-white border-1 border-solid border-white rounded-full cursor-pointer transition-all md:p-2.25 md:text-5 hover:border-primary-magenta hover:text-primary-magenta",
+	},
+}
